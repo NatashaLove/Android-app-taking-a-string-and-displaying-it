304450f000ff9d0251f0f10e7ea41dd6aee77208
Natasha S Love <debora@inbox.ru>
2018-12-12 20:05:20 -0600

    update

---
+++ app/src/main/java/com/example/debor/donothingapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.debor.donothingapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.donothingapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        //The Intent constructor takes two parameters:
+        //A Context as its first parameter (this is used because the Activity class is a subclass of Context)
+        //The Class of the app component to which the system should deliver the Intent (in this case, the activity that should be started).
+        //connects main activity with the activity in parameter
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
